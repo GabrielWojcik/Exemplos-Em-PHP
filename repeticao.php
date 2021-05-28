b5ede8cec5c6d7eb417839764c310a09a268c426
Gabriel Wojcik <62144072+GabrielWojcik@users.noreply.github.com>
2021-05-27 21:37:14 -0300

    first commit

---
+++ repeticao.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    
+
+</head>
+<body>
+
+    <h1>Repetição em PHP</h1>
+    <?php
+    $valor1 = 1;
+    $valor2 = 100;
+
+    for($i = $valor1; $i<=$valor2;$i=$i+2)
+        echo "$i<br>"
+    ?>
+
+
+</body>
+</html>
