7230a3408d534c01054ebc17a922897e90fac07c
unknown <gabrielwojcikv@gmail.com>
2021-05-27 21:28:09 -0300

    first commit

---
+++ if.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    
+
+</head>
+<body>
+
+    <h1>Operadores</h1>
+    <?php
+    $valor = 9;
+    print"Valor = $valor<br>";
+    if($valor>0)
+        print"O valor é Positivo! ";
+    
+    elseif($valor<0)
+        print"O valor é Negativo!";
+    
+    else 
+        print"O valor é Neutro!";
+    
+    ?>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    
+
+</head>
+<body>
+
+    <h1>Operadores</h1>
+    <?php
+    $n1 = 10;
+    $n2 = 2;
+    $soma = $n1+$n2;
+    $sub = $n1-$n2;
+    $mult = $n1*$n2;
+    $div = $n1/$m2;
+    $resto = $n1%$n2;
+
+    echo "Valor 1 = $n1 <br>  Valor2 = $n2 <br> Soma = $soma ";
+    echo "Subtração = $sub ";
+    echo "Multiplicação = $mult ";
+    echo "Divisão = $div ";
+    echo "Resto = $resto";
+    ?>
+
+
+</body>
+</html>
